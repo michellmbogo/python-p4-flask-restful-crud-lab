782d3c00d6a38d52ea1720c8f32971124f80b691
michellmbogo <mishie254@gmail.com>
2025-04-10 16:10:06 +0300

    flask restful

--- server/app.py
+++ server/app.py
@@ -47,6 +47,25 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.get_or_404(id) 
+        data = request.get_json() 
+        
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+        
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.get_or_404(id) 
+        
+        db.session.delete(plant)  
+        db.session.commit() 
+
+        return make_response("", 204)
+
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
